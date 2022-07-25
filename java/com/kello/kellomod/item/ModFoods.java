2af0d5b54c9520c37d35e557d8bea342132b01c0
Kello8 <109847167+Kello8@users.noreply.github.com>
2022-07-25 11:59:55 +1000

    Delete java/com/kello/kellomod directory

--- java/com/kello/kellomod/item/ModFoods.java
+++
@@ -1,9 +0,0 @@
-package com.kello.kellomod.item;
-
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.food.FoodProperties;
-
-public class ModFoods {
-    public static final FoodProperties AMETHYST_BREAD = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.9F).effect(new MobEffectInstance(MobEffects.REGENERATION, 400, 0), 0.2F).fast().build();
-}
