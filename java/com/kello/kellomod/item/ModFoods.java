461bd46e139621bfd56da36c31f48bb5434c1796
Kello <109847167+Kello8@users.noreply.github.com>
2022-07-26 10:59:05 +1000

    Update ModFoods.java

--- java/com/kello/kellomod/item/ModFoods.java
+++ java/com/kello/kellomod/item/ModFoods.java
@@ -9,5 +9,9 @@ public class ModFoods {
     public static final FoodProperties AMETHYST_APPLE =
             (new FoodProperties.Builder()).nutrition(10).saturationMod(1F).effect(new MobEffectInstance(MobEffects.REGENERATION, 2400, 0), 1F).effect(new MobEffectInstance(MobEffects.ABSORPTION, 1600, 2), 1F).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100, 2), 1F).alwaysEat().fast().build();
     public static final FoodProperties AMETHYST_CARROT = (new FoodProperties.Builder()).nutrition(7).saturationMod(15.5F).effect(new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 0.05F).build();
-    public static final FoodProperties AMETHYST_BEEF = (new FoodProperties.Builder()).nutrition(12).saturationMod(0.8F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.02F).build();
+    public static final FoodProperties AMETHYST_BEEF = (new FoodProperties.Builder()).nutrition(12).saturationMod(2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.02F).build();
+    public static final FoodProperties AMETHYST_CHICKEN = (new FoodProperties.Builder()).nutrition(8).saturationMod(2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.01F).build();
+    public static final FoodProperties AMETHYST_BEETROOT = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.1F).build();
+    public static final FoodProperties AMETHYST_MUTTON = (new FoodProperties.Builder()).nutrition(8).saturationMod(2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.01F).build();
+    public static final FoodProperties AMETHYST_POTATO = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.9F).effect(new MobEffectInstance(MobEffects.REGENERATION, 20, 4), 0.01F).build();
 }
